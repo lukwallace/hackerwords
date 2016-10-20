501e8eb7ec67ee391d868f9b6595d2d0f776ffdc
Hack Reactor Students <accounts@hackreactor.com>
2016-10-19 19:38:58 -0700

    Add logic to clicks on board to prevent repeat letters and non-adjacent letter combos.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,22 +2,75 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     var context = this;
+
+    this.rowSize = 4;
+
     this.state = {
-      'curWord': 'ggg',
+      'curWord': '',
       'curIndexesUsed': []
+    }
+
+    this.getLastClickIndex = function() {
+      if(this.state.curIndexesUsed.length === 0) {
+        return null;
+      }
+      return this.state.curIndexesUsed[this.state.curIndexesUsed.length - 1];
+    }
+
+    this.getClickIndexNumber = function(ci) {
+      return Number(ci.slice(1));
+    }
+
+    this.isInUsedIndexes = (clickIndex) => {
+      return (this.state.curIndexesUsed.indexOf(clickIndex) !== -1);
     }
 
     this.boardClick = (event) => {
       var clickLetter = event.target.innerHTML;
       var clickIndex = event.target.className;
-      var newCurWord = context.state.curWord + clickLetter;
-      var newCurIndexes = context.state.curIndexesUsed.concat(clickIndex);
-      context.setState({
-        'curWord': newCurWord,
-        'curIndexesUsed': newCurIndexes
-      });
-      console.log('curWord: ', context.state.curWord);
-      console.log('newCurWord: ', newCurWord);
+
+
+      //check adjacent
+      var isAdjacent = () => {
+        var lastClick = this.getLastClickIndex();
+        if(lastClick === null) {
+          return true;
+        }
+        var lastClickIndex = this.getClickIndexNumber(lastClick);
+        var thisClickIndex = this.getClickIndexNumber(clickIndex);
+
+        var lastClickRow = Math.floor(lastClickIndex / this.rowSize);
+        var lastClickCol = (lastClickIndex % this.rowSize);
+        var thisClickRow = Math.floor(thisClickIndex / this.rowSize);
+        var thisClickCol = (thisClickIndex % this.rowSize);
+        
+        if(Math.abs(lastClickRow - thisClickRow) > 1) {
+          return false;
+        }
+        if(Math.abs(lastClickCol - thisClickCol) > 1) {
+          return false;
+        }
+        return true;
+      };
+
+      if(clickIndex === this.getLastClickIndex()) {
+        console.log('word finalized as ', context.state.curWord);
+        //evaluate word here
+        context.setState({
+          'curWord': '',
+          'curIndexesUsed': []
+        });
+      } else {
+        if(isAdjacent() && !this.isInUsedIndexes(clickIndex)) {
+          var newCurWord = context.state.curWord + clickLetter;
+          var newCurIndexes = context.state.curIndexesUsed.concat(clickIndex);
+          //re-evaluate using mutable objects as state params?
+          context.setState({
+            'curWord': newCurWord,
+            'curIndexesUsed': newCurIndexes
+          });
+        }
+      }
     };
   }
 
@@ -28,7 +81,7 @@ class App extends React.Component {
           <Score />
           <Board boardStr={'abcdefghijklmnop'} clickHandler={this.boardClick}/>
           <Timer />
-          <CurrentWord curWord={this.state.curWord}/>
+          Current Word: {this.state.curWord}
           <PlayedWords />
         </div>
       );
