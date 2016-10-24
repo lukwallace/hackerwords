3d0c873af30c18e84076222b2729d1fff735e830
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-10-23 21:32:16 -0700

    Add before each

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
 const supertest = require('supertest');
 const server = require('../server/server.js');
+const User = require('./userModel.js');
 
 const request = supertest.agent(server);
 
@@ -13,5 +14,15 @@ describe('Meta Testing', () => {
 describe('Server', () => {
   it('Should respond with html given a non-api request', (done) => {
     request.get('/').expect(200, /<html/, done);
+  });
+
+  describe('Server-Database Interaction', () => {
+    beforeEach((done) => {
+
+
+    });
+    afterEach();
+
+    it('');
   });
 });
