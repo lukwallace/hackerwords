a34626c868e08a4ebb9ca94bfb7c9fe09ca64d94
Franco Abaroa <franco9395@gmail.com>
2016-10-26 15:56:46 -0700

    Board style changes. Change score placement

--- client/gameComponents/App.jsx
+++ client/gameComponents/App.jsx
@@ -150,7 +150,7 @@ class App extends React.Component {
     // check adjacent
     const isAdjacent = () => {
       const lastClick = this.getLastClickIndex();
-      
+
       if (lastClick === null) {
         return true;
       }
@@ -209,7 +209,7 @@ class App extends React.Component {
   // }
 
 // <div onClick={Util.signOut(this)}>Sign Out</div>
-  
+
   //could use this to add a callback for set state
   //However, its unclear how to set it with Enzyme
   setState(stateObj) {
@@ -226,11 +226,12 @@ class App extends React.Component {
         <div className="gameBoardApp" />
         <h1> HackerWords </h1>
         <div className='timeLeft'>{this.state.timeLeft}</div>
+        <div><Score score={this.state.score} /></div>
         <div className='currentWord'>{this.state.curWord}</div>
         <Board boardStr={this.state.boardStr} clickHandler={this.boardClick} />
         <PlayedWords wordsPlayed={this.state.wordsPlayed} wordScores={this.state.wordScores} />
         <button onClick={this.logOut}> Sign Out </button>
-        <div><Score score={this.state.score} /></div>
+
       </div>
     );
   }
